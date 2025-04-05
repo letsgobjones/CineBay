6ca5de7b0f1d06a3811f79ea0862cc90d61e4bc0
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-05 23:36:28 +0900

    Created PreviewContainer.

---
+++ CineBay/PreviewContainer.swift
@@ -0,0 +1,24 @@
+//
+//  PreviewContainer.swift
+//  CineBay
+//
+//  Created by Brandon Jones on 4/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct PreviewContainer {
+    static var shared: ModelContainer = {
+        let schema = Schema([
+            Movie.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        do {
+            let container = try ModelContainer(for: schema, configurations: modelConfiguration)
+            return container
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+}

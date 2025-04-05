52610869292c69b519aac775ec26d285db3085ef
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-05 23:04:30 +0900

    Created MovieStore.

---
+++ CineBay/ViewModels/MovieStore.swift
@@ -0,0 +1,24 @@
+//
+//  MovieStore.swift
+//  CineBay
+//
+//  Created by Brandon Jones on 4/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+
+@Observable
+final class MovieStore {
+  
+  private var modelContext: ModelContext
+  
+  init(modelContext: ModelContext) {
+    self.modelContext = modelContext
+  }
+  
+  
+  
+  
+}

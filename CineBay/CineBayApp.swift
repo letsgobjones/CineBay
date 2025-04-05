5809f86b9ad9ad0f24f17a64d103ea01bc304e1c
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-05 21:16:12 +0900

    Initial Commit

---
+++ CineBay/CineBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineBayApp.swift
+//  CineBay
+//
+//  Created by Brandon Jones on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CineBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

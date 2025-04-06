adbf9d8d9db748b02f32b2159a57829a3c1e79bf
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-06 19:04:40 +0900

    Implemented an update button on MovieDetailScreen

--- CineBay/Screens/MovieDetailScreen.swift
+++ CineBay/Screens/MovieDetailScreen.swift
@@ -9,9 +9,26 @@ import SwiftUI
 import SwiftData
 
 struct MovieDetailScreen: View {
+  
+  @Environment(MovieStore.self) private var movieStore
   let movie: Movie
+  
+  @State private var title: String = ""
+  @State private var year: Int?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      Form {
+        TextField("Title", text: $title)
+        TextField("Year", value: $year, format: .number)
+        Button("Udpate") {
+          guard let year = year else { return }
+          movie.title = title
+          movie.year = year
+          movieStore.saveChanges()
+        }
+      }.onAppear {
+       title = movie.title
+        year = movie.year
+      }
     }
 }
 

b2c3ac3e91d92884fc845f60586012cde1a4a37f
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-07 07:39:50 +0900

    Created AddReviewScreen

--- CineBay/Screens/Reviews/AddReviewScreen.swift
+++ CineBay/Screens/Reviews/AddReviewScreen.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct AddReviewScreen: View {
+  @Environment(\.dismiss) private var dismiss
+  @Environment(MovieStore.self) private var movieStore
+  
+  let movie: Movie
+  
+  @State private var subject: String = ""
+  @State private var  description: String = ""
+  
+  
+  private var isFormValid: Bool {
+!subject.isEmptyOrWhiteSpace && !description.isEmptyOrWhiteSpace
+  }
+  
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      Form {
+        TextField("Subject", text: $subject)
+        TextField("Description", text: $description)
+   
+        
+        
+      }.navigationTitle("Add Review")
+        .toolbar {
+          ToolbarItem(placement: .topBarLeading) {
+            Button("Close"){
+              dismiss()
+            }
+          }
+          ToolbarItem(placement: .topBarTrailing) {
+            Button("Save") {
+              
+            }.disabled(!isFormValid)
+          }
+          
+        }
     }
 }
 
 #Preview {
-    AddReviewScreen()
+  let movieStore = MovieStore(modelContext: PreviewContainer.shared.mainContext)
+  NavigationStack {
+    AddReviewScreen(movie: Movie.example[0])
+  }
+  .environment(movieStore)
+  .modelContainer(PreviewContainer.shared)
 }

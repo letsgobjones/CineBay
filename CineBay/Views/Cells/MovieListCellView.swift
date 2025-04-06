cfbc93c7b0246d0179d38e74a62f0ebd3ffe2321
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-06 19:24:26 +0900

    Created MovieListCellView

---
+++ CineBay/Views/Cells/MovieListCellView.swift
@@ -0,0 +1,28 @@
+//
+//  MovieListCellView.swift
+//  CineBay
+//
+//  Created by Brandon Jones on 4/6/25.
+//
+
+import SwiftUI
+
+struct MovieListCellView: View {
+  
+  var movie: Movie
+  
+    var body: some View {
+      HStack {
+        Text(movie.title)
+        Spacer()
+        Text(movie.year.description)
+      }
+    }
+}
+
+#Preview {
+  NavigationStack {
+    MovieListCellView(movie: .example.first!)
+      .padding()
+  }
+}

2dbbbd8bdbcbd06b68da2fd196c483786f70b65b
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-06 18:16:47 +0900

    Created MovieDetailScreen

--- CineBay/Screens/MovieDetailScreen.swift
+++ CineBay/Screens/MovieDetailScreen.swift
@@ -6,13 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MovieDetailScreen: View {
+  let movie: Movie
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MovieDetailScreen()
+  let movieStore = MovieStore(modelContext: PreviewContainer.shared.mainContext)
+  NavigationStack {
+    MovieDetailScreen(movie: Movie.example.first!)
+  }
+  .modelContainer(PreviewContainer.shared)
+  .environment(movieStore)
 }

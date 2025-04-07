28a299b2dd62d8e38c8fbf36801223307e768db8
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-08 08:28:35 +0900

    Created ActorCellView

---
+++ CineBay/Views/Cells/ActorCellView.swift
@@ -0,0 +1,25 @@
+//
+//  ActorCellView.swift
+//  CineBay
+//
+//  Created by Brandon Jones on 4/8/25.
+//
+
+import SwiftUI
+
+struct ActorCellView: View {
+  
+  let actor: Actor
+    var body: some View {
+      VStack(alignment: .leading) {
+        Text(actor.name)
+        Text((actor.movies?.map { $0.title }).flatMap { $0.formatted(.list(type: .and)) } ?? "No Movies Listed")
+      }
+    }
+}
+
+#Preview {
+  ActorCellView(actor: Actor.example[0])
+  
+    .padding()
+}

527c8f59d73818a8188f374a7cb9ee6d0e557b1f
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-08 17:42:54 +0900

    Created MovieSelectionView

---
+++ CineBay/Views/Selection/MovieSelectionView.swift
@@ -0,0 +1,51 @@
+//
+//  MovieSelectionView.swift
+//  CineBay
+//
+//  Created by Brandon Jones on 4/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MovieSelectionView: View {
+  @Environment(AppManager.self) private var appManager
+  
+  @Query(sort: \Movie.title, order: .forward) private var movies: [Movie]
+  
+  @Binding var selectedMovies: Set<Movie>
+  
+    var body: some View {
+      List(movies) { movie in
+        HStack {
+          Image(systemName: selectedMovies.contains(movie) ? "checkmark.square" :"square")
+            .onTapGesture {
+              if !selectedMovies.contains(movie) {
+               selectedMovies.insert(movie)
+              } else {
+                selectedMovies.remove(movie)
+              }
+              
+            }
+          Text(movie.title)
+        }
+        
+      }
+    }
+}
+
+#Preview {
+  let context = PreviewContainer.shared.mainContext
+  let appManager = AppManager(modelContainer: PreviewContainer.shared)
+  
+  for movie in Movie.example {
+    context.insert(movie)
+  }
+  try! context.save()
+  
+  return NavigationStack {
+    MovieSelectionView(selectedMovies: .constant(Set<Movie>()))
+  }
+  .environment(appManager)
+  .modelContainer(PreviewContainer.shared)
+}

c4b60de7b6bf53b0821ee293fc74b44c5ba51fa0
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-04-09 09:07:01 +0900

    Created MovieEnums

---
+++ CineBay/Enums/MovieEnums.swift
@@ -0,0 +1,22 @@
+//
+//  MovieEnums.swift
+//  CineBay
+//
+//  Created by Brandon Jones on 4/9/25.
+//
+
+enum MovieFilterOption: Identifiable, Hashable {
+  case none
+  case titleContains(String)
+//  case yearIs(Int)
+  
+  var id: Self { self }
+  }
+
+
+enum MovieSortOption: Identifiable, Hashable {
+  case byTitle
+  case byYear
+  
+  var id: Self { self }
+}
